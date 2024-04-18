e2499a3295701ded797d1997b9277cee116c976a
leoalvesviana <leonardo.viana@meta3.com.br>
2024-04-18 11:25:20 -0300

    E-Commerce/main Ajustando DbSet de todas as entidades

--- E-Commerce/E-Commerce.Persistence/Context/AppDbContext.cs
+++ E-Commerce/E-Commerce.Persistence/Context/AppDbContext.cs
@@ -7,4 +7,11 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Address> Addresses { get; set; }
 }
